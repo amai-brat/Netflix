224a2230f05ab090d54aa8e8384be53e1805d0cd
Nigmatyanov Ilnur <ilnurnigmatanov97@gmail.com>
2024-03-15 17:53:36 +0300

    основная часть личного кабинета

--- Frontend/src/Pages/PersonalAccount/GeneralPart/GeneralPart.jsx
+++ Frontend/src/Pages/PersonalAccount/GeneralPart/GeneralPart.jsx
@@ -1,9 +1,46 @@
-import { useState } from 'react'
+import {useEffect, useState} from 'react'
+import './styles/background.css'
+import {useLocation} from "react-router-dom";
+import './styles/content.css'
+import './styles/tabContent.css'
 
-const GeneralPart = (Component) => {
+const GeneralPart = ({component: Component}) => {
+    const [currentTab, setCurrentTab] = useState(0);
+    const tabs = ["Личные данные", "Избранное", "Подписки", "Рецензии"];
+    const location = useLocation();
+    const setInitialTab = () => {
+        const pathSegments = location.pathname.split('/');
+        const accountSection = pathSegments[pathSegments.indexOf('PersonalAccount') + 1];
+        const tabNames = ["PersonalInfoTab", "FavouritesTab", "SubscriptionsTab", "PersonalReviewsTab"];
+
+        const tabIndex = tabNames.findIndex(name => name.toLowerCase() === accountSection?.toLowerCase());
+        if (tabIndex !== -1) {
+            setCurrentTab(tabIndex);
+        }
+    };
+    useEffect(() => {
+        setInitialTab();
+    }, [location]);
+    function tabClicked(index) {
+        setCurrentTab(index);
+    }
     return (
         <>
-            <Component/>
+            <div className={"background"}>
+                <div className={"content"}>
+                    <div className={"navigation"}>
+                        {tabs.map((tab, index) =>
+                            <div className={`tab ${index === currentTab ? 'active' : ''}`}
+                                 onClick={() => tabClicked(index)}>
+                                {tab}
+                            </div>
+                        )}
+                    </div>
+                    <div className={"tabContent"}>
+                        <Component/>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
