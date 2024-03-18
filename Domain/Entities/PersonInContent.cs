f6383e3b3429955268ccfab3fa2629d364947f03
Maxim Lozhkin <maximlozhking2004@gmail.com>
2024-03-18 13:03:10 +0300

    Поправил табуляцию

--- Domain/Entities/PersonInContent.cs
+++ Domain/Entities/PersonInContent.cs
@@ -3,9 +3,9 @@
 public class PersonInContent
 {
     public int Id { get; set; }
-
-	public long ContentId { get; set; }
-	public ContentBase Content { get; set; }
+    
+    public long ContentId { get; set; }
+    public ContentBase Content { get; set; }
 
     public string Name { get; set; } = null!;
 
