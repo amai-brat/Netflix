7c92257637e3ac61ee78f2012f4656cd67c184dc
Maxim Lozhkin <maximlozhking2004@gmail.com>
2024-03-20 14:24:13 +0300

    Добавил роль к сущности пользователь

--- Domain/Entities/User.cs
+++ Domain/Entities/User.cs
@@ -4,6 +4,7 @@ public class User
 {
     public long Id { get; set; }
     public string Nickname { get; set; } = null!;
+    public string Role { get; set; } = null!;
     public string? ProfilePictureUrl { get; set; }
     public DateOnly? BirthDay { get; set; }
     public string Email { get; set; } = null!;
