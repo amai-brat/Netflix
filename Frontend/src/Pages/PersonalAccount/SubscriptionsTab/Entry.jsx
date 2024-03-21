672317d16e406b35082d760d0a93ddca5c1cb790
Nigmatyanov Ilnur <ilnurnigmatanov97@gmail.com>
2024-03-21 14:37:59 +0300

    сделал визуал, без событий

---
+++ Frontend/src/Pages/PersonalAccount/SubscriptionsTab/Entry.jsx
@@ -0,0 +1,36 @@
+﻿import {useState} from "react";
+import styles from "./styles/styles.module.css";
+const Entry = ({data}) => {
+    const [isOpened, setIsOpened] = useState(false)
+    const stateStyles = {
+        transform: `rotate(${isOpened ? 180 : 0}deg)`
+    }
+    function openSubscription(){
+        setIsOpened(!isOpened)
+    }
+    return(
+        <>
+            
+            <div className={styles.folded}>
+                <label >
+                    <input type={"checkbox"} onClick={() => openSubscription()}/>
+                    <span className={styles.buttonUnfold}
+                    style={stateStyles}></span>
+                </label>
+                <span className={styles.top}>{data.name}</span>
+                <span className={styles.bottom}>Куплено: {data.boughtAt}</span>
+            </div>
+            <div className={styles.unfolded} style={{display: isOpened ? "block" : "none"}}>
+                <div className={styles.line}></div>
+                <span>Истекает: {data.expiresAt}</span>
+                <ul>
+                    {data.info.map((value, index) =>
+                        <li key={index}>{value}</li>
+                    )}
+                </ul>
+                <button className={styles.denyButton}>Отказаться</button>
+            </div>
+        </>
+    );
+};
+export default Entry;
