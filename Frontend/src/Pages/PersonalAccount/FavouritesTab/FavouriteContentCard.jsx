6394479d97c8d684a07dcc54bb7d6b0046cc32d1
DmitriyAlexv <dmitriy_aleksandrov_2004@bk.ru>
2024-03-21 17:09:08 +0300

    Добавил дефолтную картинку при не найденном постере

--- Frontend/src/Pages/PersonalAccount/FavouritesTab/FavouriteContentCard.jsx
+++ Frontend/src/Pages/PersonalAccount/FavouritesTab/FavouriteContentCard.jsx
@@ -1,14 +1,20 @@
 import {useLocation, useNavigate} from "react-router-dom";
+import {useState} from "react";
 
 const FavouriteContentCard = ({content, userScore, addedAt}) => {
     const navigate = useNavigate()
+    const [contentImg, setContentImg] = useState(content.icon.toString())
+
+    const setDefaultContentImg = () => {
+        setContentImg("/src/assets/NoImage.svg")
+    }
     const navigateToViewContent = (id) => {
         navigate("/ViewContent" + id)
     }
     
     return(
         <div className="favourite-content-card">
-            <img className="favourite-content-card-poster" src={content.PosterUrl.toString()} alt="Poster"/>
+            <img className="favourite-content-card-poster" src={contentImg} alt="Poster" onError={setDefaultContentImg}/>
             <div className="favourite-content-card-data" onClick={() => {navigateToViewContent(content.Id)}}>
                 <label className="favourite-content-card-data-name">{content.Name}</label>
                 <div className="favourite-content-card-data-personal-data">
