48f1988c59fccb1887c0b6dc7af2fc35a6275cec
Maxim Lozhkin <maximlozhking2004@gmail.com>
2024-03-22 20:59:53 +0300

    Добавил сущности "Подписка" свойство "Описание"
    Не стал делать массив строк, в Ef core это как-то непонятно работает. Легче и правильней, чтобы хранилось описание обычным текстом

--- Domain/Entities/Subscription.cs
+++ Domain/Entities/Subscription.cs
@@ -4,6 +4,7 @@ public class Subscription
 {
     public int Id { get; set; }
     public string Name { get; set; } = null!;
+    public string Description { get; set; } = null!;
 
     public List<ContentBase> AccessibleContent { get; set; } = null!;
 }
