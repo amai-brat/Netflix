1a1006df2e92b3d195f1a59acb31be1d1611ddbd
DmitriyAlexv <dmitriy_aleksandrov_2004@bk.ru>
2024-03-24 11:20:51 +0300

    Изменил карточку контнента

--- Frontend/src/Pages/SelectionContent/SelectionContentCard.jsx
+++ Frontend/src/Pages/SelectionContent/SelectionContentCard.jsx
@@ -1,13 +1,19 @@
 import {useNavigate} from "react-router-dom";
+import {useState} from "react";
 
 const SelectionContentCard = ({content}) => {
     const navigate = useNavigate()
+    const [poster, setPoster] = useState(content.PosterUrl)
+
+    const setPosterDefault = () => {
+        setPoster("/src/assets/NoImage.svg")
+    }
     const navigateToViewContent = () => {
         navigate("/ViewContent/" + content.Id)
     }
     return(
         <div key={content.Id} className="selection-content-card" onClick={navigateToViewContent}>
-            <img className="selection-content-card-poster" src={content.PosterUrl} alt="Poster"/>
+            <img className="selection-content-card-poster" src={poster} alt="Poster" onError={setPosterDefault} style={{backgroundColor:"white"}}/>
             <label className="selection-content-card-name">{content.Name}</label>
         </div>
     )
