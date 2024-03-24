62c99a70a0f67014dc01828c933da711b00953bd
DmitriyAlexv <dmitriy_aleksandrov_2004@bk.ru>
2024-03-24 14:26:48 +0300

    Собрал страницу

--- Frontend/src/Pages/SelectionContent/SelectionContent.jsx
+++ Frontend/src/Pages/SelectionContent/SelectionContent.jsx
@@ -1,8 +1,34 @@
 import { useState } from 'react'
+import SelectionContentGrid from "./SelectionContentGrid.jsx";
+import SelectionContentFilterPanel from "./SelectionContentFilterPanel.jsx";
+
+
 
 const SelectionContent = () => {
+    const [filter, setFilter] =useState({
+        name: null,
+        type: null,
+        genres: [],
+        releaseYearFrom : null,
+        releaseYearTo : null,
+        ratingFrom : null,
+        ratingTo : null
+    })
+    
     return (
-        <></>
+        <div>
+            <div>
+                <SelectionContentGrid
+                    contents={[{Id: 1, Name: "asd", PosterUrl: "dsd"}]}
+                />
+            </div>
+            <div>
+                <SelectionContentFilterPanel
+                    filter={filter}
+                    setFilter={setFilter}
+                />
+            </div>
+        </div>
     )
 }
 
