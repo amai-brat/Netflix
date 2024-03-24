e203b508ab7b32a6b48016932b66425dd7ffea85
DmitriyAlexv <dmitriy_aleksandrov_2004@bk.ru>
2024-03-24 11:20:38 +0300

    Добавил тестовой информации

---
+++ Frontend/src/Pages/SelectionContent/TestData.jsx
@@ -0,0 +1,52 @@
+export const genres = [
+    {
+        Id: 1,
+        Name: "Боевик"
+    },
+    {
+        Id: 2,
+        Name: "Драма"
+    },
+    {
+        Id: 3,
+        Name: "Фентези"
+    },
+    {
+        Id: 4,
+        Name: "Биография"
+    },
+    {
+        Id: 5,
+        Name: "Приключение"
+    },
+    {
+        Id: 6,
+        Name: "Ужас"
+    },
+    {
+        Id: 7,
+        Name: "Триллер"
+    },
+    {
+        Id: 8,
+        Name: "Комедия"
+    },
+    {
+        Id: 9,
+        Name: "Детектив"
+    }
+]
+export const contentTypes = [
+    {
+        Id: 1,
+        ContentType: "Фильм"
+    },
+    {
+        Id: 2,
+        ContentType: "Мультфильм"
+    },
+    {
+        Id: 3,
+        ContentType: "Сериал"
+    }
+]
