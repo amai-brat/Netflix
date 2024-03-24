4a4fd9ea06d7001709fe62d5ba6e9a6325d9bb44
DmitriyAlexv <dmitriy_aleksandrov_2004@bk.ru>
2024-03-24 19:01:09 +0300

    Поправил логику фильтров

--- Frontend/src/Pages/SelectionContent/SelectionContentContentTypeFilter.jsx
+++ Frontend/src/Pages/SelectionContent/SelectionContentContentTypeFilter.jsx
@@ -1,4 +1,5 @@
 import "/src/Pages/SelectionContent/Styles/SelectionContentContentTypeFilter.css";
+import {useState} from "react";
 
 const SelectionContentContentTypeFilter = ({filter, contentTypes}) => {
     const ContentTypesFilter = () => {
@@ -8,14 +9,15 @@ const SelectionContentContentTypeFilter = ({filter, contentTypes}) => {
             return <label className="selection-content-info-label">Что-то пошло не так</label>
         } else{
             return contentTypes.map((type, index) => {
-                    const typesIds = filter.types.map((type) => type.Id)
+                    const [checked, setChecked] = useState(filter.types.includes(type.Id))
                     return (
                         <div key={index}>
-                            <input className="selection-content-filter-panel-type-name-cb" type="checkbox" checked={typesIds.includes(type.Id)} onChange={(e) => {
+                            <input className="selection-content-filter-panel-type-name-cb" type="checkbox" checked={checked} onChange={(e) => {
+                                setChecked(!checked)
                                 if(e.target.checked) {
-                                    filter.types.append(type.Id)
+                                    filter.types.push(type.Id)
                                 }else {
-                                    filter.types.remove(type.Id)
+                                    filter.types = filter.types.filter((id) => id !== type.Id)
                                 }
                             }}/>
                             <label className="selection-content-filter-panel-type-name">{type.ContentType}</label>
