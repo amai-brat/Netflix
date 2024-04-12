74f87d7ab8c2c1fed4d64928767e9ff5df635a02
DmitriyAlexv <dmitriy_aleksandrov_2004@bk.ru>
2024-04-12 16:26:43 +0300

    Добавил сущность уведомлений в БД

--- DataAccess/AppDbContext.cs
+++ DataAccess/AppDbContext.cs
@@ -25,6 +25,7 @@ namespace DataAccess
 		public DbSet<UsersReviews> UsersReviews => Set<UsersReviews>();
 		public DbSet<Review> Reviews => Set<Review>();
 		public DbSet<Comment> Comments => Set<Comment>();
+		public DbSet<CommentNotification> CommentNotifications => Set<CommentNotification>();
 
 		public DbSet<Subscription> Subscriptions => Set<Subscription>();
 		public DbSet<Currency> Currencies => Set<Currency>();
@@ -34,6 +35,7 @@ namespace DataAccess
 		protected override void OnModelCreating(ModelBuilder modelBuilder)
 		{
 			modelBuilder.ApplyConfiguration(new CommentEntityConfiguration());
+			modelBuilder.ApplyConfiguration(new CommentNotificationEntityConfiguration());
 			modelBuilder.ApplyConfiguration(new ContentBaseEntityConfiguration());
 			modelBuilder.ApplyConfiguration(new ContentTypeEntityConfiguration());
 			modelBuilder.ApplyConfiguration(new EpisodeEntityConfiguration());
