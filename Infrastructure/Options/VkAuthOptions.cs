4b0028815d814db77da115857907fe6881bc49a4
DmitriyAlexv <dmitriy_aleksandrov_2004@bk.ru>
2024-05-01 18:18:16 +0300

    Add options

--- Infrastructure/Options/VkAuthOptions.cs
+++ Infrastructure/Options/VkAuthOptions.cs
@@ -2,5 +2,9 @@ namespace Infrastructure.Options;
 
 public class VkAuthOptions
 {
-    
+    public string AuthUri { get; set; } = null!;
+    public string TokenUri { get; set; } = null!;
+    public string RedirectUri { get; set; } = null!;
+    public string ClientId { get; set; } = null!;
+    public string ClientSecret { get; set; } = null!;
 }
