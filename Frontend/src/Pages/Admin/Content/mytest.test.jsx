a11c396df9babbe3f98731d89b0a1193cf422e97
Nigmatyanov Ilnur <ilnurnigmatanov97@gmail.com>
2024-09-25 14:44:50 +0300

    первый коммит

---
+++ Frontend/src/Pages/Admin/Content/mytest.test.jsx
@@ -0,0 +1,8 @@
+﻿import { render, screen } from '@testing-library/react';
+import AdminContent from './AdminContent';
+import { test, expect } from 'vitest';
+
+test('renders MyComponent', () => {
+    render(<AdminContent/>);
+    expect(screen.getByText('Добавить фильм')).toBeInTheDocument();
+});
--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,16 +1,23 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    watch: {
-      usePolling: true,
+        plugins: [react()],
+        server: {
+            watch: {
+                usePolling: true,
+            },
+            strictPort: true,
+            host: process.env.VITE_HOST || "0.0.0.0",
+            port: process.env.VITE_PORT || 5173,
+            // hmr: {
+            //   clientPort: process.env.VITE_CLIENT_PORT || null
+            // },
+        },
+        test: {
+            environment: 'jsdom',
+            globals: true,
+            setupFiles: './setupTests.js'
+        }
     },
-    strictPort: true,
-    host: process.env.VITE_HOST || "0.0.0.0",
-    port: process.env.VITE_PORT || 5173,
-    // hmr: {
-    //   clientPort: process.env.VITE_CLIENT_PORT || null
-    // },
-  }})
+)
