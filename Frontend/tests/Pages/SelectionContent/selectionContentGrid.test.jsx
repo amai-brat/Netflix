bd509c572e1ee53cbe38535321cda0499aa352d0
DmitriyAlexv <dmitriy_aleksandrov_2004@bk.ru>
2024-09-27 17:17:18 +0300

    Поправка

--- Frontend/tests/Pages/SelectionContent/selectionContentGrid.test.jsx
+++ Frontend/tests/Pages/SelectionContent/selectionContentGrid.test.jsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import { describe, test, expect } from "vitest";
+import { describe, test, expect, vi } from "vitest";
 import SelectionContentGrid from "../../../src/Pages/SelectionContent/SelectionContentGrid.jsx";
 
 vi.mock("../../../src/Pages/SelectionContent/SelectionContentCard.jsx", () => {
