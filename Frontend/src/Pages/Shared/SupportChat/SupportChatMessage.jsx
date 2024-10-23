1fed6a20796076b58d56931748b48a040a329f83
DmitriyAlexv <dmitriy_aleksandrov_2004@bk.ru>
2024-10-23 14:40:02 +0300

    Исправил стили

--- Frontend/src/Pages/Shared/SupportChat/SupportChatMessage.jsx
+++ Frontend/src/Pages/Shared/SupportChat/SupportChatMessage.jsx
@@ -3,6 +3,7 @@ import "/src/Pages/Shared/SupportChat/Styles/SupportChatMessage.css"
 const SupportChatMessage = ({message}) => {
     return(
         <div className={"support-chat-message " + message.role}>
+            <label style={{fontSize: "12px", textAlign: (message.role === "support" ? "left": "right")}}>{message.role === "support" ? "Поддержка" : "Вы"}</label>
             <label>{message.text.trim()}</label>
         </div>
     )
