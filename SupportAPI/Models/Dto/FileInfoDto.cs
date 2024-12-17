481d486a555c67f54d2c2d060a1dcef6bcf33730
Nigmatyanov Ilnur <ilnurnigmatanov97@gmail.com>
2024-12-17 15:09:30 +0300

    warning fixes

--- SupportAPI/Models/Dto/FileInfoDto.cs
+++ SupportAPI/Models/Dto/FileInfoDto.cs
@@ -3,8 +3,11 @@ namespace SupportAPI.Models.Dto;
 
 public class FileInfoDto
 {
+    // ReSharper disable once UnusedAutoPropertyAccessor.Global
     public required string Type { get; set; }
+    // ReSharper disable once UnusedAutoPropertyAccessor.Global
     public required Uri Src { get; set; }
+    // ReSharper disable once UnusedAutoPropertyAccessor.Global
     public required string Name { get; set; }
     // public string? Metadata { get; set; }
 }
