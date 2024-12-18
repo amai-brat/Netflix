b3eb4378efe86c87aa0863723c29427064534588
Nigmatyanov Ilnur <ilnurnigmatanov97@gmail.com>
2024-12-18 11:09:05 +0300

    убрал хардкод

--- SupportPermanentS3Service/Controllers/UploadController.cs
+++ SupportPermanentS3Service/Controllers/UploadController.cs
@@ -1,13 +1,16 @@
 ﻿using System.Security.Claims;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using SupportPermanentS3Service.Options;
 using SupportPermanentS3Service.Services;
 
 namespace SupportPermanentS3Service.Controllers;
 
 [ApiController]
 public class UploadController(
-    IFileCopyService fileCopyService): ControllerBase
+    IFileCopyService fileCopyService,
+    IOptions<MinioProxyOptions> proxyOptions): ControllerBase
 {
     
     [HttpPost("copy-to-perm")]
@@ -32,13 +35,19 @@ public class UploadController(
             return Unauthorized();
         }
         var uri = await fileCopyService.GetPresignedUriAsync(guid, cancellationToken);
+        var options = proxyOptions.Value;
+        Console.WriteLine(options.Scheme);
+        Console.WriteLine(options.Host);
+        Console.WriteLine(options.Port);
+        Console.WriteLine(options.RoutingKey);
         var uriRewrite = new UriBuilder(uri)
         {
-            Scheme = "http",
-            Host = "localhost",
-            Port = 9000,
-            Path = "/perm-s3" + uri.AbsolutePath
+            Scheme = options.Scheme,
+            Host = options.Host,
+            Port = options.Port,
+            Path = options.RoutingKey + uri.AbsolutePath
         }.Uri;
+        Console.WriteLine(uriRewrite.ToString());
         return Ok(uriRewrite);
     }
 }
