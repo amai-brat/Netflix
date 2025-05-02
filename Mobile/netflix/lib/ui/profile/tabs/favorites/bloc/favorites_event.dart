470b5354773f77a8eaaae3fb7ad10442cc224543
DmitriyAlexv <dmitriy_aleksandrov_2004@bk.ru>
2025-05-02 23:03:26 +0300

    Добавление bloc для избранного

---
+++ Mobile/netflix/lib/ui/profile/tabs/favorites/bloc/favorites_event.dart
@@ -0,0 +1,33 @@
+import 'package:netflix/domain/models/favorite_filter_params.dart';
+
+abstract class FavoriteEvent {
+  const FavoriteEvent();
+}
+
+class SearchQueryChanged extends FavoriteEvent {
+  final String query;
+  const SearchQueryChanged(this.query);
+}
+
+class SortChanged extends FavoriteEvent {
+  final FavoritesSortBy sort;
+  const SortChanged(this.sort);
+}
+
+class LoadInitialData extends FavoriteEvent {}
+
+class LoadFavorite extends FavoriteEvent {}
+
+class UpdateFilterParams extends FavoriteEvent {
+  final FavoriteFilterParams Function(FavoriteFilterParams currentParams) updateFn;
+  const UpdateFilterParams(this.updateFn);
+}
+
+class ApplyFilters extends FavoriteEvent {
+  final FavoriteFilterParams params;
+  const ApplyFilters(this.params);
+}
+
+class ResetFilters extends FavoriteEvent {}
+
+class ResetSort extends FavoriteEvent {}
