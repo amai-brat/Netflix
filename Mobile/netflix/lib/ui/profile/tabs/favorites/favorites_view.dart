887e21ce15f9653c0667dc106794006917d1e895
DmitriyAlexv <dmitriy_aleksandrov_2004@bk.ru>
2025-05-02 23:04:58 +0300

    Изменение вкладки избранного

--- Mobile/netflix/lib/ui/profile/tabs/favorites/favorites_view.dart
+++ Mobile/netflix/lib/ui/profile/tabs/favorites/favorites_view.dart
@@ -1,20 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:netflix/domain/use_cases/get_favorite_by_filter_use_case.dart';
+import 'package:netflix/ui/profile/tabs/favorites/bloc/favorites_bloc.dart';
+import 'package:netflix/ui/profile/tabs/favorites/bloc/favorites_event.dart';
+import 'package:netflix/ui/profile/tabs/favorites/components/content/favorite_list.dart';
+import 'package:netflix/ui/profile/tabs/favorites/components/search_sort_row.dart';
+import 'package:netflix/utils/app_colors.dart';
+import 'package:netflix/utils/di.dart';
 
-class FavoritesView extends StatefulWidget {
+class FavoritesView extends StatelessWidget {
   const FavoritesView({super.key});
 
   @override
-  State<StatefulWidget> createState() => _FavoritesViewState();
-}
-
-class _FavoritesViewState extends State<FavoritesView> {
-  final check = UniqueKey();
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Избранное')),
-      body: Center(child: Text('$check')),
+    return BlocProvider(
+      create: (context) => FavoriteBloc(
+          getFavoriteByFilterUseCase: locator<GetFavoriteByFilterUseCase>(),
+      )..add(LoadInitialData()),
+      child: Scaffold(
+          appBar: AppBar(
+            title: const Text('Персональная информация'),
+            backgroundColor: AppColors.inputGrey,
+          ),
+          backgroundColor: AppColors.backgroundBlack,
+          body: Column(
+            children: [
+              SearchSortRow(),
+              Expanded(child: FavoriteList()),
+            ],
+          )
+      ),
     );
   }
 }
