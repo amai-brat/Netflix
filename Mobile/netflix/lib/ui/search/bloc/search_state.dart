a44819d4a191dd303c0f3c04bb8ab490f31bfd84
DmitriyAlexv <dmitriy_aleksandrov_2004@bk.ru>
2025-05-02 12:54:10 +0300

    Добавление логики bloc для страницы поиска

---
+++ Mobile/netflix/lib/ui/search/bloc/search_state.dart
@@ -0,0 +1,63 @@
+import 'package:netflix/domain/models/content.dart';
+import 'package:netflix/domain/models/content_filter_params.dart';
+import 'package:netflix/domain/models/content_type.dart';
+import 'package:netflix/domain/models/genre.dart';
+
+class SearchState {
+  final List<Content> contents;
+  final int page;
+  final int perPage;
+  final bool hasMore;
+  final bool isLoadingMore;
+  final List<Genre> availableGenres;
+  final List<ContentType> availableTypes;
+  final List<String> availableCountries;
+  final bool isLoading;
+  final bool isInit;
+  final String error;
+  final ContentFilterParams filterParams;
+
+  const SearchState({
+    this.contents = const [],
+    this.page = 0,
+    this.perPage = 8,
+    this.hasMore = true,
+    this.isLoadingMore = false,
+    this.availableGenres = const [],
+    this.availableTypes = const [],
+    /// Прикольно, что мы так и не реализовали репозиторий для стран на бэке (во фронте это просто хардкодом XD)
+    this.availableCountries = const ['Не выбрано', 'Россия', 'США', 'Франция', 'Корея'],
+    this.isLoading = false,
+    this.isInit = false,
+    this.error = '',
+    required this.filterParams,
+  });
+
+  SearchState copyWith({
+    List<Content>? contents,
+    int? page,
+    bool? hasMore,
+    bool? isLoadingMore,
+    List<Genre>? availableGenres,
+    List<ContentType>? availableTypes,
+    bool? isLoading,
+    bool? isInit,
+    String? error,
+    ContentFilterParams? filterParams,
+  }) {
+    return SearchState(
+      contents: contents ?? this.contents,
+      page: page ?? this.page,
+      perPage: this.perPage,
+      hasMore: hasMore ?? this.hasMore,
+      isLoadingMore: isLoadingMore ?? this.isLoadingMore,
+      availableGenres: availableGenres ?? this.availableGenres,
+      availableTypes: availableTypes ?? this.availableTypes,
+      availableCountries: this.availableCountries,
+      isLoading: isLoading ?? this.isLoading,
+      isInit: isInit ?? this.isInit,
+      error: error ?? this.error,
+      filterParams: filterParams ?? this.filterParams,
+    );
+  }
+}
