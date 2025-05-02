c8489787b75fd2241823a1bfcf45443dc797482b
DmitriyAlexv <dmitriy_aleksandrov_2004@bk.ru>
2025-05-02 23:06:55 +0300

    Уброка ненужного

--- Mobile/netflix/lib/domain/repositories/content_repository.dart
+++ Mobile/netflix/lib/domain/repositories/content_repository.dart
@@ -2,6 +2,5 @@ import 'package:netflix/domain/models/content.dart';
 import 'package:netflix/domain/models/content_filter_params.dart';
 
 abstract class ContentRepository {
-  ///
   Future<List<Content>> getContent(ContentFilterParams params, int page, int perPage);
 }
