cadb1c32b2b7de4522f46bd0358dabe05c36c2f9
Almaz Shamilov <pepegapeepoclown4head@gmail.com>
2025-05-04 19:41:42 +0300

    серый appBar

--- Mobile/netflix/lib/utils/app_theme.dart
+++ Mobile/netflix/lib/utils/app_theme.dart
@@ -14,7 +14,7 @@ class AppTheme {
         backgroundColor: Color(0xFFE50914),
         foregroundColor: Color(0xFFFFFFFF),
         disabledBackgroundColor: Color(0xFF0BB000),
-        disabledForegroundColor: Color(0xFFFFFFFF)
+        disabledForegroundColor: Color(0xFFFFFFFF),
       ),
     ),
     inputDecorationTheme: InputDecorationTheme(
@@ -26,6 +26,9 @@ class AppTheme {
         borderRadius: BorderRadius.all(Radius.circular(25)),
         borderSide: BorderSide.none,
       ),
+    ),
+    appBarTheme: AppBarTheme(
+      backgroundColor: Color(0xFF303030)
     ),
   );
 
