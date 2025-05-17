72624fa4e271d036018966a9da902f3950bf446c
DmitriyAlexv <dmitriy_aleksandrov_2004@bk.ru>
2025-05-17 17:32:10 +0300

    Изменил реализацию репозитория ContentType

---
+++ Mobile/netflix/lib/data/models/content_type_dto.dart
@@ -0,0 +1,14 @@
+import 'package:netflix/domain/models/content/content_type.dart';
+
+class ContentTypeDto {
+  final int id;
+  final String name;
+
+  const ContentTypeDto({required this.id, required this.name});
+
+  ContentTypeDto.fromMap(Map<String, dynamic> map)
+      : id = map['id'],
+        name = map['name'];
+
+  ContentType toContentType() => ContentType(id: id, name: name);
+}
---
+++ Mobile/netflix/lib/data/repositories/content_type_repository_impl.dart
@@ -0,0 +1,36 @@
+import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:netflix/data/models/content_type_dto.dart';
+import 'package:netflix/domain/models/content/content_type.dart';
+import 'package:netflix/domain/repositories/content_type_repository.dart';
+
+class ContentTypeRepositoryImpl extends ContentTypeRepository {
+  final GraphQLClient _client;
+
+  ContentTypeRepositoryImpl(this._client);
+
+  @override
+  Future<List<ContentType>> getContentTypes() async {
+    const query = r'''
+      query GetContentTypes() {
+        contentTypes {
+          id
+          name
+        }
+      }
+    ''';
+
+    final options = QueryOptions(
+      document: gql(query),
+    );
+
+    final result = await _client.query(options);
+
+    if (result.hasException) {
+      final error = result.exception!.linkException ?? result.exception!.graphqlErrors.map((e) => e.message).join('\n');
+      throw Exception(error);
+    }
+
+    final contentTypes = result.data?['contentTypes'] ?? [];
+    return (contentTypes as List).map((json) => ContentTypeDto.fromMap(json).toContentType()).toList();
+  }
+}
