fd3edee69ec02f680d99f93d0ae150fcc2796055
DmitriyAlexv <dmitriy_aleksandrov_2004@bk.ru>
2025-05-23 21:58:30 +0300

    Изменение логики чата на более потокобезопасную в SupportAPI.csproj;

--- SupportAPI/Services/Implementations/ChatSessionManager.cs
+++ SupportAPI/Services/Implementations/ChatSessionManager.cs
@@ -7,20 +7,17 @@ namespace SupportAPI.Services.Implementations;
 
 public class ChatSessionManager<T>: IChatSessionManager<T> where T: class
 {
-    private readonly ConcurrentDictionary<long, List<string>> _userSessions = new();
+    private readonly ConcurrentDictionary<long, ConcurrentDictionary<string, object?>> _userSessions = new();
     private readonly ConcurrentDictionary<string, IServerStreamWriter<T>?> _sessionStreams = new();
-    private readonly ConcurrentDictionary<long, List<string>> _userGroups = new();
+    private readonly ConcurrentDictionary<long, ConcurrentDictionary<string, object?>> _userGroups = new();
     
     public virtual string CreateUserSession(long userId)
     {
         var sessionId = Guid.NewGuid().ToString();
         
-        if (!_userSessions.ContainsKey(userId))
-        {
-            _userSessions.TryAdd(userId, []);
-        }
+        _userSessions.GetOrAdd(userId, []);
         
-        _userSessions[userId].Add(sessionId);
+        _userSessions[userId].TryAdd(sessionId, null);
         _sessionStreams.TryAdd(sessionId, null);
         CreateUserGroup(userId, sessionId);
         return sessionId;
@@ -39,7 +36,7 @@ public class ChatSessionManager<T>: IChatSessionManager<T> where T: class
         if (!_userSessions.TryGetValue(userId, out var value)) 
             return;
         
-        value.Remove(sessionId);
+        value.TryRemove(sessionId, out _);
         _sessionStreams.TryRemove(sessionId, out _);
         LeaveUserSessionGroup(userId, sessionId);
             
@@ -51,10 +48,8 @@ public class ChatSessionManager<T>: IChatSessionManager<T> where T: class
 
     public virtual void JoinUserSessionGroup(long ownerId, string sessionId)
     {
-        if (!_userGroups.TryGetValue(ownerId, out var _)) 
-            _userGroups.TryAdd(ownerId, []);
-            
-        _userGroups[ownerId].Add(sessionId);
+        _userGroups.GetOrAdd(ownerId, []);
+        _userGroups[ownerId].TryAdd(sessionId, null);
     }
 
     public virtual void LeaveUserSessionGroup(long ownerId, string sessionId)
@@ -62,7 +57,7 @@ public class ChatSessionManager<T>: IChatSessionManager<T> where T: class
         if (!_userGroups.TryGetValue(ownerId, out var group)) 
             return;
         
-        group.Remove(sessionId);
+        group.TryRemove(sessionId, out _);
         
         if (group.IsNullOrEmpty())
             RemoveUserGroup(ownerId);
@@ -70,18 +65,25 @@ public class ChatSessionManager<T>: IChatSessionManager<T> where T: class
 
     public bool IsSessionBelongToUser(long userId, string sessionId) =>
         _userSessions.TryGetValue(userId, out var userSessions) &&
-        userSessions.Any(userSessionId => userSessionId == sessionId);
+        userSessions.Any(userSessionId => userSessionId.Key == sessionId);
 
     public virtual async Task BroadcastMessageToUserGroupAsync(long ownerId, T message, params string[] excludeSessionIds)
     {
         if (_userGroups.TryGetValue(ownerId, out var group))
         {
-            foreach (var sessionId in group.Where(sId => 
-                         excludeSessionIds == null || !excludeSessionIds.Contains(sId)))
+            foreach (var sessionId in group.ToArray().Where(sId => 
+                         excludeSessionIds == null || !excludeSessionIds.Contains(sId.Key)))
             {
-                if (_sessionStreams.TryGetValue(sessionId, out var stream) && stream != null)
+                if (!_sessionStreams.TryGetValue(sessionId.Key, out var stream) || stream == null) 
+                    continue;
+                
+                try
                 {
                     await stream.WriteAsync(message);
+                }
+                catch
+                {
+                    // ignored
                 }
             }
         }
@@ -91,20 +93,24 @@ public class ChatSessionManager<T>: IChatSessionManager<T> where T: class
     {
         foreach (var sessionId in sessionIds)
         {
-            if (_sessionStreams.TryGetValue(sessionId, out var stream) && stream != null)
+            if (!_sessionStreams.TryGetValue(sessionId, out var stream) || stream == null) 
+                continue;
+            
+            try
             {
                 await stream.WriteAsync(message);
+            }
+            catch
+            {
+                // ignored
             }
         }
     }
     
     private void CreateUserGroup(long userId, string sessionId)
     {
-        if (!_userGroups.ContainsKey(userId))
-        {
-            _userGroups.TryAdd(userId, []);
-        }
-        _userGroups[userId].Add(sessionId);
+        _userGroups.GetOrAdd(userId, []);
+        _userGroups[userId].TryAdd(sessionId, null);
     }
 
     private void RemoveUserGroup(long userId)
