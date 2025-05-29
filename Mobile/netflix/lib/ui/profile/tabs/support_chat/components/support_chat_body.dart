24ea9fa120ebb68a96c42af91432a7f585bc09d4
DmitriyAlexv <dmitriy_aleksandrov_2004@bk.ru>
2025-05-29 19:55:29 +0300

    Добавление компонента чата

---
+++ Mobile/netflix/lib/ui/profile/tabs/support_chat/components/support_chat_body.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:netflix/ui/profile/tabs/support_chat/bloc/support_chat_bloc.dart';
+import 'package:netflix/ui/profile/tabs/support_chat/bloc/support_chat_state.dart';
+import 'package:netflix/ui/profile/tabs/support_chat/components/support_chat_files_preview/support_chat_files_preview.dart';
+import 'package:netflix/ui/profile/tabs/support_chat/components/support_chat_messages/support_chat_message_list.dart';
+
+class SupportChatBody extends StatelessWidget{
+  const SupportChatBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<SupportChatBloc, SupportChatState>(
+      builder: (context, state) {
+        if (state is SupportChatLoading) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (state is SupportChatError) {
+          return Center(child: Text(state.message));
+        } else if (state is SupportChatConnected) {
+          return Column(
+            children: [
+              const SupportChatFilesPreview(),
+              Expanded(child: SupportChatMessageList(messages: state.messages)),
+            ],
+          );
+        }
+        return const SizedBox.shrink();
+      },
+    );
+  }
+}
